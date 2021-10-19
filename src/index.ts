16f63a307beb19975913cec58c86fb84e75a1472
Rowan de Haas <rowandh@users.noreply.github.com>
2021-10-19 17:32:30 +1300

    Add serialization

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,96 @@ export enum Prefix {
 
 type MethodParameterValue = number | bigint | string | Buffer | boolean;
 
+export const OP_CREATECONTRACT = 0xc0;
+export const OP_CALLCONTRACT = 0xc1;
+
 export interface MethodParameter {
   type: Prefix,
   value: MethodParameterValue
 };
+
+/*
+  Accepts the input call data and serializes it to a hex string.
+*/
+export const serialize = (data: ContractTxData): string => {
+  
+  if (isCallContract(data.opCodeType)) {
+    return serializeCallContract(data);
+  }
+
+  return "";
+}
+
+const isCallContract = (opCodeType: number): boolean => {
+  return opCodeType == OP_CALLCONTRACT;
+}
+
+const serializeCallContract = (data: ContractTxData): string => {
+  /* Structure:
+    {
+      opcode byte,
+      vmVersion int (4 bytes),
+      gasPrice ulong (8 bytes),
+      gasLimit ulong (8 bytes),
+      contractAddress byte[] (20 bytes),
+      RLP encoded list: {
+        methodName,
+        methodParams
+      }
+    }
+  */
+  let opCodeByte = Buffer.alloc(1, data.opCodeType);
+  let vmVersionBytes = Buffer.alloc(4);
+  vmVersionBytes.writeInt32LE(data.vmVersion);
+
+  let gasPriceBytes = Buffer.alloc(8);
+  gasPriceBytes.writeBigUInt64LE(data.gasPrice);
+
+  let gasLimitBytes = Buffer.alloc(8)
+  gasLimitBytes.writeBigUInt64LE(data.gasLimit);
+
+  let prefix = Buffer.concat([opCodeByte, vmVersionBytes, gasPriceBytes, gasLimitBytes])
+
+  // RLP bit
+  let serializedMethodName = Buffer.from(data.methodName);
+  let serializedParams = serializeMethodParameters(data.methodParameters);
+  let callDataBytes: Buffer[] = [serializedMethodName, serializedParams];
+
+  // For some reason we double-rlp encode the call data params and then the method name and params.
+  let rlpEncodedCallData = rlp.encode(callDataBytes);
+  
+  let finalBuffer = Buffer.concat([prefix, data.contractAddress, rlpEncodedCallData]);
+
+  return finalBuffer.toString('hex');
+}
+
+const serializeCreateContract = () => {
+
+}
+
+const serializeMethodParameters = (methodParameters: MethodParameter[]): Buffer => {
+  let callDataBytes: Buffer[] = [];
+
+  for (let p of methodParameters) {
+    let serializedParam = serializeParam(p);
+    callDataBytes.push(serializedParam);
+  }
+
+  return rlp.encode(callDataBytes);
+}
+
+const serializeParam = (param: MethodParameter): Buffer => {
+  /* Structure:
+    {
+      prefix byte,
+      param byte[]
+    }
+  */
+ let prefixByte = Buffer.alloc(1, param.type);
+ let serializedBytes = serializePrimitiveValue(param);
+
+ return Buffer.concat([prefixByte, serializedBytes]);
+}
 
 /*
   Parses a hexadecimal string and interprets the smart contract data within.
@@ -122,6 +208,45 @@ export const deserializePrimitiveValue = (type: number, primitiveBytes: Buffer):
     case Prefix.UInt128:
     case Prefix.UInt256:
       return primitiveBytes; // TODO use BigNum for these types
+    default:
+      throw "Invalid type!";      
+  }
+}
+
+export const serializePrimitiveValue = (parameter: MethodParameter): Buffer => {
+  switch (parameter.type) {
+    case Prefix.Address: // Should already be a buffer
+      return <Buffer>parameter.value;
+    case Prefix.Bool:
+      return parameter.value ? Buffer.from([1]) : Buffer.from([0]);
+    case Prefix.Byte:
+    case Prefix.ByteArray:
+      return <Buffer>parameter.value;
+    case Prefix.Char:
+      let char = Buffer.alloc(2); // C# sizeof(char) is 2 bytes, the first is ASCII and the 2nd is blank
+      char.writeInt16LE((<string>parameter.value).charCodeAt(0));
+      return char;
+    case Prefix.String:
+      return Buffer.from(<string>parameter.value);
+    case Prefix.Int:
+      let int32 = Buffer.alloc(4);
+      int32.writeInt32LE(<number>parameter.value);
+      return int32;
+    case Prefix.UInt:
+      let uint32 = Buffer.alloc(4);
+      uint32.writeUInt32LE(<number>parameter.value);
+      return uint32;
+    case Prefix.Long:
+      let long = Buffer.alloc(8);
+      long.writeBigInt64LE(<bigint>parameter.value);
+      return long;
+    case Prefix.ULong:
+      let ulong = Buffer.alloc(8);
+      ulong.writeBigUInt64LE(<bigint>parameter.value);
+      return ulong;
+    case Prefix.UInt128:
+    case Prefix.UInt256:
+      return <Buffer>parameter.value; // TODO use BigNum for these types
     default:
       throw "Invalid type!";      
   }
--- test/calldata.test.ts
+++ test/calldata.test.ts
@@ -1,4 +1,4 @@
-import { parse, Prefix } from '../src';
+import { parse, serialize, Prefix, ContractTxData, OP_CREATECONTRACT, MethodParameter, OP_CALLCONTRACT } from '../src';
 
 describe('deserialize', () => {
 
@@ -19,6 +19,157 @@ describe('deserialize', () => {
     expect(txData.gasLimit).toEqual(BigInt("0x" + gasLimit));
     expect(txData.contractAddress).toEqual(Buffer.from(contractAddress, "hex"));
     expect(txData.methodName).toEqual("Execute");
+  });
+
+  it('should have the correct method params', () => {
+    // vmversion 1, gasprice 1, gaslimit 18446744073709551615, method name
+    /*
+        {
+            object[] methodParameters =
+            {
+                true,
+                (byte)1,
+                Encoding.UTF8.GetBytes("test"),
+                's',
+                "test",
+                (int)int.MaxValue,
+                (uint)uint.MaxValue,
+                (long)long.MaxValue,
+                (ulong)ulong.MaxValue,
+                UInt128.MaxValue,
+                UInt256.MaxValue,
+                "0x95D34980095380851902ccd9A1Fb4C813C2cb639".HexToAddress()
+            };
+    */
+    let gasLimit = "ffffffffffffffff";
+    let contractAddress = "6400000000000000000000000000000000000000";
+    let hex = "c1010000000100000000000000ffffffffffffffff6400000000000000000000000000000000000000f88c8745786563757465b882f880820101820201850a74657374830373008504746573748506ffffff7f8505ffffffff8908ffffffffffffff7f8907ffffffffffffffff910bffffffffffffffffffffffffffffffffa10cffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff950995d34980095380851902ccd9a1fb4c813c2cb639";
+    let txData = parse(hex)
+    let opcode = 193;
+
+    expect(txData.opCodeType).toEqual(opcode);
+    expect(txData.gasPrice).toEqual(BigInt(1));
+    expect(txData.gasLimit).toEqual(BigInt("0x" + gasLimit));
+    expect(txData.contractAddress).toEqual(Buffer.from(contractAddress, "hex"));
+    expect(txData.methodName).toEqual("Execute");
+    expect(txData.methodParameters[0].type).toEqual(Prefix.Bool);
+    expect(txData.methodParameters[0].value).toEqual(true);
+    expect(txData.methodParameters[1].type).toEqual(Prefix.Byte);
+    expect(txData.methodParameters[1].value).toEqual(Buffer.from([1]));
+    expect(txData.methodParameters[2].type).toEqual(Prefix.ByteArray);
+    expect(txData.methodParameters[2].value).toEqual(Buffer.from("test", "utf8"));
+    expect(txData.methodParameters[3].type).toEqual(Prefix.Char);
+    expect(txData.methodParameters[3].value).toEqual("s");
+    expect(txData.methodParameters[4].type).toEqual(Prefix.String);
+    expect(txData.methodParameters[4].value).toEqual("test");
+    expect(txData.methodParameters[5].type).toEqual(Prefix.Int);
+    expect(txData.methodParameters[5].value).toEqual(2147483647);
+    expect(txData.methodParameters[6].type).toEqual(Prefix.UInt);
+    expect(txData.methodParameters[6].value).toEqual(4294967295);
+    expect(txData.methodParameters[7].type).toEqual(Prefix.Long);
+    expect(txData.methodParameters[7].value).toEqual(BigInt("0x7FFFFFFFFFFFFFFF")); // long.MaxValue
+    expect(txData.methodParameters[8].type).toEqual(Prefix.ULong);
+    expect(txData.methodParameters[8].value).toEqual(BigInt("0xFFFFFFFFFFFFFFFF")); // ulong.MaxValue
+    expect(txData.methodParameters[9].type).toEqual(Prefix.UInt128);
+    expect(txData.methodParameters[9].value).toEqual(Buffer.from("ffffffffffffffffffffffffffffffff", "hex")); // UInt128.MaxValue
+    expect(txData.methodParameters[10].type).toEqual(Prefix.UInt256);
+    expect(txData.methodParameters[10].value).toEqual(Buffer.from("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "hex")); // UInt256.MaxValue
+    expect(txData.methodParameters[11].type).toEqual(Prefix.Address);
+    expect(txData.methodParameters[11].value).toEqual(Buffer.from("95D34980095380851902ccd9A1Fb4C813C2cb639", "hex"));
+  });  
+});
+
+describe('serialize', () => {
+
+  it('should have the correct data when serializing a call', () => {
+    // vmversion 1, gasprice 1, gaslimit 18446744073709551615, method name
+    /*
+        {
+            object[] methodParameters =
+            {
+                true,
+                (byte)1,
+                Encoding.UTF8.GetBytes("test"),
+                's',
+                "test",
+                (int)int.MaxValue,
+                (uint)uint.MaxValue,
+                (long)long.MaxValue,
+                (ulong)ulong.MaxValue,
+                UInt128.MaxValue,
+                UInt256.MaxValue,
+                "0x95D34980095380851902ccd9A1Fb4C813C2cb639".HexToAddress()
+            };
+    */
+   
+    let gasprice = "01000000000000000";
+    let gasLimit = "ffffffffffffffff";
+
+    let contractTxData = {
+      opCodeType: OP_CALLCONTRACT,
+      vmVersion: 1,
+      contractAddress: Buffer.from("6400000000000000000000000000000000000000", "hex"),
+      gasPrice: BigInt(1),
+      gasLimit: BigInt("0x" + gasLimit),
+      methodName: "Execute",
+      methodParameters: [
+        {
+          type: Prefix.Bool,
+          value: true
+        },
+        {
+          type: Prefix.Byte,
+          value: Buffer.from([1])
+        },
+        {
+          type: Prefix.ByteArray,
+          value: Buffer.from("test", "utf8")
+        },
+        {
+          type: Prefix.Char,
+          value: "s"
+        },
+        {
+          type: Prefix.String,
+          value: "test"
+        },
+        {
+          type: Prefix.Int,
+          value: 2147483647
+        },
+        {
+          type: Prefix.UInt,
+          value: 4294967295
+        },
+        {
+          type: Prefix.Long,
+          value: BigInt("0x7FFFFFFFFFFFFFFF")
+        },
+        {
+          type: Prefix.ULong,
+          value: BigInt("0xFFFFFFFFFFFFFFFF")
+        },
+        {
+          type: Prefix.UInt128,
+          value: Buffer.from("ffffffffffffffffffffffffffffffff", "hex")
+        },
+        {
+          type: Prefix.UInt256,
+          value:Buffer.from("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "hex")
+        },
+        {
+          type: Prefix.Address,
+          value: Buffer.from("95D34980095380851902ccd9A1Fb4C813C2cb639", "hex")
+        }
+      ]
+    } as ContractTxData;
+
+    let hex = serialize(contractTxData);
+    
+    let expectedHex = "c1010000000100000000000000ffffffffffffffff6400000000000000000000000000000000000000f88c8745786563757465b882f880820101820201850a74657374830373008504746573748506ffffff7f8505ffffffff8908ffffffffffffff7f8907ffffffffffffffff910bffffffffffffffffffffffffffffffffa10cffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff950995d34980095380851902ccd9a1fb4c813c2cb639";
+    
+    // Verified in FullNode
+    expect(hex).toEqual(expectedHex);
   });
 
   it('should have the correct method params', () => {
